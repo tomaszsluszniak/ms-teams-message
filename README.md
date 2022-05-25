24d426b7f83a5747e4bd94ac8a19e233f834e1e6
Tomasz Słuszniak <tomasz.sluszniak@gmail.com>
2022-05-25 20:29:39 +0200

    ms teams message

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# bps-on-demand-teams-connector
+# ms-teams-message
+
+Function-as-a-service to send a one-to-one message between a technical account and an employee. 
+You can use it as a notification channel between any application and employees.
+
+# How to set it up 
+
+Assuming that you already have the [OpenFaaS](https://github.com/openfaas/faas) (alternatively [faasd](https://github.com/openfaas/faasd)), the steps are:
+
+## 1. Prepare Azure Active Directory Application
+
+Please follow the [instruction](https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app#register-an-application). Please focus on **Register an application** only.
+
+After creating, go to **Authentication > Advanced settings > Allow public client flows** > Enable the following mobile and desktop flows:, select **Yes**.
+
+## 2. Create secrets
+
+You have to create a couple of **secrets**. 
+
+Secret name | Description  
+--- | --- 
+`ms-teams-message-az-client-id` | `Azure Active Directory Application` Client Id  
+`ms-teams-message-upn` | Your technical account `User Principal Name`
+`ms-teams-message-password` |  Your technical account password
+`ms-teams-message-sentry-dsn` | Sentry DSN. If you **don't want** to use it, just type anything.
+
+
+```bash
+faas-cli secret create ms-teams-message-az-client-id --gateway <OpenFaaS Gateway>
+faas-cli secret create ms-teams-message-upn --gateway <OpenFaaS Gateway>
+faas-cli secret create ms-teams-message-password --gateway <OpenFaaS Gateway>
+faas-cli secret create ms-teams-message-sentry-dsn --gateway <OpenFaaS Gateway>
+```
+
+## 3. Deploy the function
+
+```bash
+faas-cli deploy -f ms-teams-message.yml --gateway <OpenFaaS Gateway>
+```
+
+# How to send a message
+
+To send a message simply call the **POST** method on `http://<OpenFaaS Gateway>/function/ms-teams-message` with payload:
+
+```json
+{
+    "recipient": "employee_user_principal_name@domain.com",
+    "message": "The message body can contain HTML tags. For example you can `<b>bold some part of the message body</b>."
+}
+```
--- ms_teams_message/handler.py
+++ ms_teams_message/handler.py
@@ -1,5 +1,4 @@
 import json
-import os
 import logging
 
 import sentry_sdk
@@ -11,22 +10,14 @@ from azure.identity import UsernamePasswordCredential
 from msgraph.core import GraphClient
 
 
-AZ_CLIENT_ID_FILE = '/var/openfaas/secrets/bpsod-az-client-id'
-LOGIN_FILE = '/var/openfaas/secrets/bpsod-teams-connector-login'
-PASSWORD_FILE = '/var/openfaas/secrets/bpsod-teams-connector-password'
-SENTRY_DSN_FILE = '/var/openfaas/secrets/bpsod-teams-connector-sentry-dsn'
-
-_f_client_id = open(AZ_CLIENT_ID_FILE, 'r')
-AZ_CLIENT_ID = _f_client_id.readline()
-
-_f_login = open(LOGIN_FILE, 'r')
-LOGIN = _f_login.readline()
-
-_f_password = open(PASSWORD_FILE, 'r')
-PASSWORD = _f_password.readline()
-
-_f_sentry = open(SENTRY_DSN_FILE, 'r')
-SENTRY_DSN = _f_sentry.readline()
+with open('/var/openfaas/secrets/ms-teams-message-az-client-id', 'r') as f:
+    AZ_CLIENT_ID = f.readline()
+with open('/var/openfaas/secrets/ms-teams-message-upn', 'r') as f:
+    UPN = f.readline()
+with open('/var/openfaas/secrets/ms-teams-message-password', 'r') as f:
+    PASSWORD = f.readline()
+with open('/var/openfaas/secrets/ms-teams-message-sentry-dsn', 'r') as f:
+    SENTRY_DSN = f.readline()
 
 sentry_logging = LoggingIntegration(
     level=logging.INFO,
@@ -45,19 +36,19 @@ def handle(event, context):
     if (event.method == "POST"):
         credential = UsernamePasswordCredential(
             client_id=AZ_CLIENT_ID, 
-            username=LOGIN, 
+            username=UPN, 
             password=PASSWORD
         )
-        client = GraphClient(credential=credential)
+        graph_client = GraphClient(credential=credential)
 
         params = json.loads(event.body.decode('utf-8'))
 
         errors = []
-        owner = params.get('owner')
+        recipient = params.get('recipient')
         message = params.get('message')
 
-        if owner is None:
-            errors.append('The "owner" field is required.')
+        if recipient is None:
+            errors.append('The "recipient" field is required.')
         if message is None:
             errors.append('The "message" field is required.')
             
@@ -75,16 +66,16 @@ def handle(event, context):
                 {
                     "@odata.type": "#microsoft.graph.aadUserConversationMember",
                     "roles": ["owner"],
-                    "user@odata.bind": f"https://graph.microsoft.com/v1.0/users('{LOGIN}')"
+                    "user@odata.bind": f"https://graph.microsoft.com/v1.0/users('{UPN}')"
                 },
                 {
                     "@odata.type": "#microsoft.graph.aadUserConversationMember",
                     "roles": ["owner"],
-                    "user@odata.bind": f"https://graph.microsoft.com/v1.0/users('{owner}')"
+                    "user@odata.bind": f"https://graph.microsoft.com/v1.0/users('{recipient}')"
                 }
             ]
         }
-        chat_obj = client.post(
+        chat_obj = graph_client.post(
             '/chats',
             data=json.dumps(create_chat_body),
             headers={'Content-Type': 'application/json'}
@@ -97,7 +88,7 @@ def handle(event, context):
                 "content": message
             }
         }
-        message_obj = client.post(
+        message_obj = graph_client.post(
             f'/chats/{chat_obj.get("id")}/messages',
             data=json.dumps(send_message_body),
             headers={'Content-Type': 'application/json'}
